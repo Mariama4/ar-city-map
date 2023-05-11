43813d40911ca87bd5f2d23505dcc7cbc14ead9d
mariama4 <mariama4@mail.ru>
2023-05-11 13:09:57 +0700

    refactoring files&folders

--- js/places.js
+++ js/places.js
@@ -77,7 +77,7 @@ function renderPlaces(places) {
 		const icon = document.createElement('a-image');
 		icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
 		icon.setAttribute('name', convertObjectToPrettyString(place.tags));
-		icon.setAttribute('src', './geometka.png');
+		icon.setAttribute('src', './public/geometka.png');
 
 		// for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
 		icon.setAttribute('scale', '20 20 20');
